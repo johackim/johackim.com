7c6fdbb46d93493da3b30d886c8a2642eb2fc36d
johackim <contact@johackim.com>
2022-10-04 14:53:30 +0200

    fix: display private content

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { NextSeo, ArticleJsonLd } from 'next-seo';
 import { runSync } from '@mdx-js/mdx';
 import * as runtime from 'react/jsx-runtime';
@@ -12,9 +13,30 @@ import NewsletterWidget from '@components/newsletterWidget';
 import components from '@lib/components';
 import { compileMdxToJs } from '@lib/compile';
 import { getContent, getContents, createImage } from '@lib/utils';
+import { useAuth } from '@lib/atoms';
 
-const Page = ({ title, slug, dateUpdated, code, headings, tags }) => {
-    const { default: Content } = runSync(code, runtime);
+const Page = ({ title, slug, dateUpdated, code, headings, tags, isPrivate }) => {
+    const auth = useAuth();
+    const [privateCode, setPrivateCode] = useState(false);
+    const { default: Content } = runSync(privateCode || code, runtime);
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (!isPrivate || !auth.isLoggedIn) return;
+
+        setLoading(true);
+
+        fetch('/api/private', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${auth?.user?.token}` },
+            body: JSON.stringify({ slug }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setPrivateCode(data?.code);
+                setLoading(false);
+            });
+    }, [slug]);
 
     return (
         <section className="mt-20">
@@ -27,7 +49,11 @@ const Page = ({ title, slug, dateUpdated, code, headings, tags }) => {
                             author={process.env.NEXT_PUBLIC_SITE_AUTHOR}
                             authorUrl="/a-propos"
                         >
-                            <Content components={components} />
+                            {isLoading ? (
+                                <p>Chargement en cours...</p>
+                            ) : (
+                                <Content components={components} />
+                            )}
                         </Article>
 
                         { slug !== 'start' && !tags.includes('type/moc') && (
@@ -97,7 +123,8 @@ export const getStaticProps = async ({ params }) => {
         tags,
         title,
         headings,
-        code: tags.includes('private') ? false : code,
+        code,
+        isPrivate: /<!--\s?private\s?-->/g.test(content),
         datePublished: datePublished ? String(datePublished) : null,
         dateUpdated: dateUpdated ? String(dateUpdated) : null,
     } };
