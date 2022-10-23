170a3abb9fce21ddeaad7efc2f6bb718feb65eb4
johackim <contact@johackim.com>
2022-10-23 12:55:54 +0200

    feat: group all posts by date

--- pages/all.js
+++ pages/all.js
@@ -1,39 +1,62 @@
 import { NextSeo } from 'next-seo';
-import { Article, Progress } from '@johackim/design-system';
+import { Article, Progress, Link } from '@johackim/design-system';
 
 import DefaultLayout from '@components/defaultLayout';
 import Sidebar from '@components/sidebar';
 import NewsletterWidget from '@components/newsletterWidget';
 import { getContents, removeEmojies } from '@lib/utils';
 
-const Page = ({ contents }) => (
-    <section className="mt-20">
-        <div className="container m-auto px-4 lg:max-w-screen-lg">
-            <div className="md:grid md:grid-cols-3 md:gap-4">
-                <div className="md:col-span-2 md:border dark:border-gray-800 md:px-4 self-start">
-                    <Article
-                        title="Tous les contenus"
-                        dateUpdated={contents[0].datePublished}
-                        author={process.env.NEXT_PUBLIC_SITE_AUTHOR}
-                        authorUrl="/a-propos"
-                    >
-                        <ul>
-                            {contents.map((content) => (
-                                <li key={content.slug}>
-                                    <a href={content.slug}>{content.title}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </Article>
-                </div>
+const Page = ({ contents }) => {
+    const groupedContents = contents.reduce((acc, content) => {
+        const date = new Date(content.datePublished);
+        const month = date.toLocaleString('fr-FR', { month: 'long' });
+        const year = date.getFullYear();
+        const key = `${month} ${year}`;
 
-                <Sidebar>
-                    <NewsletterWidget />
-                </Sidebar>
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+
+        acc[key].push(content);
+
+        return acc;
+    }, {});
+
+    return (
+        <section className="mt-20">
+            <div className="container m-auto px-4 lg:max-w-screen-lg">
+                <div className="md:grid md:grid-cols-3 md:gap-4">
+                    <div className="md:col-span-2 md:border dark:border-gray-800 md:px-4 self-start">
+                        <Article
+                            title="Tous les contenus"
+                            dateUpdated={contents[0].datePublished}
+                            author={process.env.NEXT_PUBLIC_SITE_AUTHOR}
+                            authorUrl="/a-propos"
+                        >
+                            {Object.keys(groupedContents).map((key) => (
+                                <div key={key}>
+                                    <h2 className="text-2xl font-bold mb-4 capitalize">{key}</h2>
+
+                                    <ul className="list-disc list-inside">
+                                        {groupedContents[key].map(({ title, slug }) => (
+                                            <li key={slug}>
+                                                <Link href={slug}>{title}</Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            ))}
+                        </Article>
+                    </div>
+
+                    <Sidebar>
+                        <NewsletterWidget />
+                    </Sidebar>
+                </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
+};
 
 Page.getLayout = (page) => (
     <DefaultLayout>
