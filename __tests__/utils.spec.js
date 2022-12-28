f634b9af9c337590c55c6cb4cc6b168295c61a1f
johackim <contact@johackim.com>
2022-12-28 14:46:08 +0100

    fix: add github token

--- __tests__/utils.spec.js
+++ __tests__/utils.spec.js
@@ -1,5 +1,12 @@
 import 'dotenv/config';
-import { getContent, getContents, getTwitterFollowers } from '@lib/utils';
+import {
+    getContent,
+    getContents,
+    getTwitterFollowers,
+    getGithubFollowers,
+    getGithubStars,
+    getGithubRepoStars,
+} from '@lib/utils';
 
 test('Should list contents', async () => {
     const files = await getContents();
@@ -32,4 +39,25 @@ test('Should return 0 if twitter account does not exist', async () => {
     const followers = await getTwitterFollowers('__johackim');
 
     expect(followers).toEqual(0);
+});
+
+test('Should return Github followers', async () => {
+    const followers = await getGithubFollowers('johackim');
+
+    expect(typeof followers).toEqual('number');
+    expect(followers).toBeGreaterThan(100);
+});
+
+test('Should return Github stars', async () => {
+    const stars = await getGithubStars('johackim');
+
+    expect(typeof stars).toEqual('number');
+    expect(stars).toBeGreaterThan(100);
+});
+
+test('Should return Github repo stars', async () => {
+    const stars = await getGithubRepoStars('johackim/johackim.com');
+
+    expect(typeof stars).toEqual('number');
+    expect(stars).toBeGreaterThan(2);
 });
