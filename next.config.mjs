d65ab747e9ea8567d2266cb35444a795c30d86fe
johackim <contact@johackim.com>
2024-10-30 18:05:23 +0100

    fix: set temporary subscriber value

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 import fetch from 'node-fetch';
 
+const TMP_VALUE = 407;
+
 const getListmonkSubscribers = async () => {
     const { data } = await fetch(`https://listmonk.johackim.com/api/lists/${process.env.LISTMONK_LIST_ID}`, {
         headers: { Authorization: `Basic ${Buffer.from(`${process.env.LISTMONK_USERNAME}:${process.env.LISTMONK_PASSWORD}`).toString('base64')}` },
     }).then((res) => res.json()).catch(() => false);
 
-    return data?.subscriber_count || 0;
+    return data?.subscriber_count || TMP_VALUE; // https://github.com/knadh/listmonk/releases/tag/v4.0.1
 };
 
 export default async () => {
