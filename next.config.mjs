ae6e760ed4297e7e6d1d77458cf39c5e4dae4e0a
johackim <contact@johackim.com>
2025-04-28 22:54:03 +0200

    fix: update listmonk

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch';
 
-const TMP_VALUE = 415;
-
 const getListmonkSubscribers = async () => {
-    const { data } = await fetch(`https://listmonk.johackim.com/api/lists/${process.env.LISTMONK_LIST_ID}`, {
-        headers: { Authorization: `Basic ${Buffer.from(`${process.env.LISTMONK_USERNAME}:${process.env.LISTMONK_PASSWORD}`).toString('base64')}` },
+    const token = Buffer.from(`${process.env.LISTMONK_USERNAME}:${process.env.LISTMONK_PASSWORD}`).toString('base64');
+
+    const { data } = await fetch('https://listmonk.johackim.com/api/subscribers', {
+        headers: { Authorization: `Basic ${token}` },
     }).then((res) => res.json()).catch(() => false);
 
-    return data?.subscriber_count || TMP_VALUE; // https://github.com/knadh/listmonk/releases/tag/v4.0.1
+    return data?.total || 0;
 };
 
 export default async () => {
