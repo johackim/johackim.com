d4bf419f3fe80d23f818bdb7be3f1a2dde7af6eb
johackim <contact@johackim.com>
2026-05-04 15:24:17 +0200

    feat: add notes.xml rss

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
-import { createSitemap, createRss } from './lib/utils.js';
+import { createSitemap, createNotes, createRss } from './lib/utils.js';
 
 export default async (phase) => {
     if (phase === PHASE_PRODUCTION_BUILD) {
         await createSitemap();
+        await createNotes();
         await createRss();
     }
 
